4b297ee5207e826901bc29dc604a5c1d76ae82f9
Bongani Makile <60345783+mhloti@users.noreply.github.com>
2020-07-22 14:25:44 +0200

    Web App Files

---
+++ edsa_recommender.py
@@ -0,0 +1,137 @@
+"""
+
+    Streamlit webserver-based Recommender Engine.
+
+    Author: Explore Data Science Academy.
+
+    Note:
+    ---------------------------------------------------------------------
+    Please follow the instructions provided within the README.md file
+    located within the root of this repository for guidance on how to use
+    this script correctly.
+
+    NB: !! Do not remove/modify the code delimited by dashes !!
+
+    This application is intended to be partly marked in an automated manner.
+    Altering delimited code may result in a mark of 0.
+    ---------------------------------------------------------------------
+
+    Description: This file is used to launch a minimal streamlit web
+	application. You are expected to extend certain aspects of this script
+    and its dependencies as part of your predict project.
+
+	For further help with the Streamlit framework, see:
+
+	https://docs.streamlit.io/en/latest/
+
+"""
+# Streamlit dependencies
+import streamlit as st
+
+# Data handling dependencies
+import pandas as pd
+import numpy as np
+import pickle
+
+# Custom Libraries
+from utils.data_loader import load_movie_titles
+from recommenders.collaborative_based import collab_model
+from recommenders.content_based import content_model
+
+# Data Loading
+title_list = load_movie_titles('resources/data/movies.csv')
+
+# App declaration
+def main():
+
+    # DO NOT REMOVE the 'Recommender System' option below, however,
+    # you are welcome to add more options to enrich your app.
+    page_options = ["Recommender System","Solution Overview"]
+
+    # -------------------------------------------------------------------
+    # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
+    # -------------------------------------------------------------------
+    page_selection = st.sidebar.selectbox("Choose Option", page_options)
+    if page_selection == "Recommender System":
+        # Header contents
+        st.write('# Movie Recommender Engine')
+        st.write('### EXPLORE Data Science Academy Unsupervised Predict')
+        st.image('resources/imgs/Image_header.png',use_column_width=True)
+        # Recommender System algorithm selection
+        sys = st.radio("Select an algorithm",
+                       ('Content Based Filtering',
+                        'Collaborative Based Filtering'))
+
+        # User-based preferences
+        st.write('### Enter Your Three Favorite Movies')
+        movie_1 = st.selectbox('First Option',title_list[14930:15200])
+        movie_2 = st.selectbox('Second Option',title_list[25055:25255])
+        movie_3 = st.selectbox('Third Option',title_list[21100:21200])
+        fav_movies = [movie_1,movie_2,movie_3]
+
+        # Perform top-10 movie recommendation generation
+        if sys == 'Content Based Filtering':
+            if st.button("Recommend"):
+                try:
+                    with st.spinner('Crunching the numbers...'):
+                        top_recommendations = content_model(movie_list=fav_movies,
+                                                            top_n=10)
+                    st.title("We think you'll like:")
+                    for i,j in enumerate(top_recommendations):
+                        st.subheader(str(i+1)+'. '+j)
+                except:
+                    st.error("Oops! Looks like this algorithm does't work.\
+                              We'll need to fix it!")
+
+
+        if sys == 'Collaborative Based Filtering':
+            if st.button("Recommend"):
+                try:
+                    with st.spinner('Crunching the numbers...'):
+                        top_recommendations = collab_model(movie_list=fav_movies,
+                                                           top_n=10)
+                    st.title("We think you'll like:")
+                    for i,j in enumerate(top_recommendations):
+                        st.subheader(str(i+1)+'. '+j)
+                except:
+                    st.error("Oops! Looks like this algorithm does't work.\
+                              We'll need to fix it!")
+
+
+    # -------------------------------------------------------------------
+
+    # ------------- SAFE FOR ALTERING/EXTENSION -------------------
+    if page_selection == "Solution Overview":
+        st.title("Solution Overview")
+        st.write("Describe your winning approach on this page")
+       
+        st.title("EDA")
+        st.write("""
+        A summary of the insights in the movies data set.
+                """)
+        
+        # Reads in saved graphs
+        
+        st.image('resources/imgs/bar.png',use_column_width=True)
+        st.write("""
+        The bar chart describes the number of ratings in millions for each 
+        movie in the database scaled from a rating of 0.5 to 5.0. The lowest 
+        rated movies are given a 0.5 rating and the highest rated movies are
+        given a 4.0. On average the ratings are 3.0 and above indicating
+        that the users enjoy the movies in the data set.        
+                 """)
+        
+        st.image('resources/imgs/fig.png',use_column_width=True)
+        st.write("""
+        The line chart describes a count of the number of ratings for each movie.
+        The shape of the chart indicates a negative relationship between the count
+        ratings and the number of movies. This means that a few movies have a 
+        higher count of ratings, and as the number of movies increase the count of
+        ratings decreases.""")
+
+    # You may want to add more sections here for aspects such as an EDA,
+    # or to provide your business pitch.
+
+
+if __name__ == '__main__':
+    main()
