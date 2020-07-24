19ddf30b9f3f101c054f452ec22f2e96a431c3d5
Bongani Makile <60345783+mhloti@users.noreply.github.com>
2020-07-24 02:39:09 +0200

    Update of the web app
    Update of the web app and additional images to enhance the look  and feel of the app.

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -32,6 +32,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+from PIL import Image
 
 # Custom Libraries
 from utils.data_loader import load_movie_titles
@@ -106,47 +107,83 @@ def main():
         st.title("Solution Overview")
         st.write("This page describes the winning approach.")
         
-        solution_options = ["Problem Statement", "Exploratory Data Analysis"]
+        solution_options = ["Datasets", "Exploratory Data Analysis"]
         
         solution_select = st.sidebar.selectbox("Choose Option", solution_options)
+        
+        if solution_select == "Datasets":
+            st.markdown("### **_Datasets_**")
+            st.write("""
+            A snapshot data sets used in the training of the model.
+                    """)
+            st.markdown("#### **_Train Data_**")
+            st.write(pd.DataFrame({"userId": [5163, 106343, 146790],
+                                  "movieId": [57669, 5, 5459],
+                                  "rating": [4.0, 4.5, 5.0],
+                                  "timestamp": [1518349992, 1206238739, 1076215539]}))
+            
+            st.markdown("#### **_Test Data_**")
+            st.write(pd.DataFrame({"userId": [1, 1, 1],
+                                  "movieId": [2011, 4144, 5767]}))
                        
         if solution_select == "Exploratory Data Analysis":
-            st.subheader("Exploratory Data Analysis")
+            st.markdown("### **_Exploratory Data Analysis_**")
             st.write("""
             A summary of the insights in the movies data set.
                     """)
 
             # Reads in saved graphs
-            st.image('resources/imgs/bar.png', use_column_width = True)
-            st.write("""
-            The bar chart describes the number of ratings in millions for each 
+            bar = """The bar chart describes the number of ratings in millions for each 
             movie in the database scaled from a rating of 0.5 to 5.0. The lowest 
             rated movies are given a 0.5 rating and the highest rated movies are
             given a 4.0. On average the ratings are 3.0 and above indicating
-            that the users enjoy the movies in the data set.        
-                     """)
-
-            st.image('resources/imgs/fig.png', use_column_width = True)
-            st.write("""
+            that the users enjoy the movies in the data set."""
+            
+            st.image('resources/imgs/bar.png', caption = bar,  use_column_width = True)
+            
+            fig = """
             The line chart describes a count of the number of ratings for each movie.
             The shape of the chart indicates a negative relationship between the count
             ratings and the number of movies. This means that a few movies have a 
             higher count of ratings, and as the number of movies increase the count of
-            ratings decreases.""")
+            ratings decreases."""
+            st.image('resources/imgs/fig.png', caption = fig, use_column_width = True)
         
     if page_selection == "About Us":
         st.title("About Us")
         
-        about_options = ["Team", "Mission Statement", "Problem Landscape", "Motivation"]
+        about_options = ["Team", "Our Mission", "Problem Statement", 
+                         "Problem Landscape", "Motivation"]
         
         about_select = st.sidebar.selectbox("Choose Option", about_options)
 
         if about_select == "Team":
-            st.subheader("Team")
-            st.write("We are a team of data scientists from Team_3_CPT.")
-       
-        if about_select == "Mission Statement":
-            st.subheader("Our Mission")
+            st.markdown("### **_ Our Data Scientists_**")
+                       
+            siya = Image.open('resources/imgs/siya.jpg')
+            viwe = Image.open('resources/imgs/viwe.jpeg') 
+            mj = Image.open('resources/imgs/mj.jpg') 
+            tendi = Image.open('resources/imgs/tendi.jpg')
+            b = Image.open('resources/imgs/b.png')
+            
+            st.image([siya, mj, tendi], 
+                     caption = ["Siyasanga", "MJ", "Tendani"],
+                     width = 150)
+            st.image([viwe, b], caption = ["Siviwe", "Bongani"],
+                     width = 150)
+            
+            st.markdown("### **_ Learn more about Team_3_CPT_**")
+            st.write("""Like mystery, Siyasanga creates wonder and wonder is the basis of 
+                     her desire to understand.""")
+            st.write("""Free spirited and adaptable are the truest words that describe 
+                     Siviwe. A little something for rainbows and sunny skies to envy.""")
+            st.write("Optimism and hardwork is what drives MJ to become a data scientist.")
+            st.write("The most friendliest person alive... a simple guy and a simple motto.")
+            st.write(""""All I want is to be a real boy in front of a computer begging
+                    my program to run." - Bongani""")
+        
+        if about_select == "Our Mission":
+            st.markdown("### **_Our Mission_**")
             st.write("""
             We are a creative and passionate group of data scientists who are
             on a mission to make a difference in our community by bringing
@@ -155,7 +192,7 @@ def main():
             giving people tools and information to make better decisions.""")
 
         if about_select == "Problem Statement":
-            st.subheader("Problem Statement")
+            st.markdown("### **_Problem Statement_**")
             st.write("""
             To construct a recommendation algorithm based on content or 
             collaborative filtering, capable of accurately predicting how
@@ -163,16 +200,17 @@ def main():
             historical preferences.""")
             
         if about_select == "Problem Landscape":
-            st.subheader("Problem Landscape")
-            st.image('resources/imgs/cpt.jpg', use_column_width = True)
-            st.write("""
+            st.markdown("### **_Problem Landscape_**")
+            
+            cpt = """
             Providing an accurate and robust solution to this challenge has 
             immense economic potential, with users of the system being 
             exposed to content they would like to view or purchase - 
-            generating revenue and platform affinity.""")
+            generating revenue and platform affinity."""
+            st.image('resources/imgs/cpt.jpg', caption = cpt, use_column_width = True)
 
         if about_select == "Motivation":
-            st.subheader("Motivation")
+            st.markdown("### **_Motivation_**")
             st.write("""Improve customer experience by exposing users to 
             content that matches their taste.""")
             st.write("""Increase sales for service providers.""")
@@ -181,13 +219,15 @@ def main():
         
     if page_selection == "Contact Us":
         st.title("Contact Us")
-        st.write("""Have any questions? We would love to hear from you.""")
+        st.markdown("### **_Have any questions? We would love to hear from you._**")
            
         st.text_input("""Name:""")
         st.text_input("""Surname:""")
-        st.text_input("""Comment:""")
+        st.text_input("""Email:""")
+        st.text_area("""Comment:""")
+        st.button("""Submit""")
         
-        st.subheader("Contact Info")
+        st.markdown("### **_Contact Info_**")
         st.write("""Tel: 012 589 4856""")
         st.write("""Fax: 012 589 4800""")
         st.write("""Email: info@team3cpt.com""")
@@ -196,8 +236,10 @@ def main():
                  South Africa""")
 
         st.subheader("Social Media")
+        st.write("""LinkedIn: Team_3_CPT""")
         st.write("""Facebook: Team_3_CPT""")
-        st.write("""Twitter & Instagram: @team_3_cpt""")
+        st.write("""Instagram: @team_3_cpt""")
+        st.write("""Twitter: @team_3_cpt""")
         
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
